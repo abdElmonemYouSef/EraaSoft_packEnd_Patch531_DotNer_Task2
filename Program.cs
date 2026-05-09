f1f934143d2f48091e47e39bedb1a521f3d46e62
abdElmonem <monamjusaf13@gmail.com>
2026-05-09 23:54:13 +0300

    some feature added to the app

--- Program.cs
+++ Program.cs
@@ -7,8 +7,14 @@
 
 
 
+            List<int> UsedList = new();
 
-            List<int> list = new List<int>() { 1, 2, 9, 4, 5, 6, 7, 8, 3 };
+            List<int> list = new List<int>() { 1,2,3,4,5,6,7,8,9 };
+
+
+            List<int> usrMemberList = new List<int>();
+
+
 
             bool UserWillToContinue = true;
 
@@ -18,7 +24,19 @@
                 Console.WriteLine(" \nplease select an option form the listed Options below : \n");
                 Console.ForegroundColor = ConsoleColor.White;
 
-                Console.WriteLine("    P - Print numbers\r\n    A - Add a number\r\n    M - Display mean of the numbers\r\n    S - Display the smallest number\r\n    L - Display the largest number\r\n    SA - Sort the list Asendently \r\n    sd - Sort the list Destently \r\n    Q - Quit\r\n");
+                Console.WriteLine("G - generate My own list\r\n" +
+                    "G - generate list\r\n" +
+                    "D - display values\r\n" +
+                    "A - Add a number\r\n" +
+                    "M - Display mean of the numbers\r\n" +
+                    "SM - Display the smallest number\r\n" +
+                    "P - Print numbers\r\n" +
+                    "L - Display the largest number\r\n" +
+                    "S - Search for value\r\n" +
+                    "SA - Sort the list Asendently \r\n" +
+                    "SD - Sort the list Destently \r\n " +
+                    "C - Clear List \r\n " +
+                    "Q - Quit\r\n");
 
                 string UserOption = Console.ReadLine();
 
@@ -27,12 +45,89 @@
                 {
 
 
+                    case "g":
+                        Console.WriteLine("please Enter the list member count : ");
 
+                        int usrMemberListCount = Convert.ToInt32(Console.ReadLine());
 
-
-
-                    case "p":
+                        for (int i = 0; i < usrMemberListCount
+                            ; i++)
                         {
+
+                            Console.WriteLine($" please Enter a value for list member no {i + 1} ");
+                            int usrMemberListValue = Convert.ToInt32(Console.ReadLine());
+
+                            usrMemberList.Add(usrMemberListValue);
+
+
+
+                        }
+
+                        Console.Write("Your List Has been added successfully :    ");
+
+                        Console.Write("[");
+                        for (int i = 0; i < usrMemberList.Count; i++)
+                        {
+                            Console.Write(usrMemberList[i]);
+                            if (i < usrMemberList.Count - 1)
+                                Console.Write(",");
+                        }
+
+                        Console.Write("]");
+
+
+
+
+
+
+
+
+
+                        break;  //generate list
+
+                    case "d": // display
+
+                        Console.WriteLine(" would you like to use you own list or use our bulit in list :");
+                        Console.WriteLine("1- will use my own one ");
+                        Console.WriteLine("2- use puilt in app list ");
+
+                        string usroption = Console.ReadLine();
+
+                        if (usroption.ToLower() == "1")
+                        {
+                            UsedList = usrMemberList;
+                            if (usrMemberList.Count == 0)
+                                Console.WriteLine("[] - the list is empty ");
+                            else
+                            {
+                                Console.Write("[");
+                                foreach (var item in UsedList)
+                                {
+
+                                    Console.Write($" {item} ");
+                                }
+                                Console.Write("]");
+
+                            }
+
+                        }
+                        else if (usroption.ToLower() == "2")
+                        {
+                            UsedList = list;
+
+                            if (list.Count == 0)
+                                Console.WriteLine("[] - the list is empty ");
+                            else
+                            {
+                                Console.Write("[");
+                                foreach (var item in UsedList)
+                                {
+
+                                    Console.Write($" {item} ");
+                                }
+                                Console.Write("]");
+
+                            }
 
 
                             foreach (var item in list)
@@ -40,22 +135,41 @@
 
                                 Console.Write($" {item}\t");
                             }
-                        }
-                        break;
 
-                    case "a":
+
+                        }
+
+
+
+                        break;   //diplay values
+
+                    case "a": // add value 
+
                         Console.WriteLine(" please Enter the value to add : ");
                         int UserAddedValueInput = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("the value is : " + UserAddedValueInput);
 
 
                         list.Add(UserAddedValueInput);
-                        foreach (var item in list)
-                        {
-                            Console.Write($"{item}\t");
-                        }
-                        break;
 
+                        Console.Write("[");
+                        Console.Write(list[1]);
+
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            Console.Write($"{list[i]}");
+
+                            if (i < list.Count - 1)
+                            {
+                                Console.Write(",");
+                            }
+                        }
+                        Console.Write("]");
+
+
+
+
+                        break; // add value
 
                     case "m":
                         int sum = 0;
@@ -66,9 +180,9 @@
                         decimal Mean = sum / list.Count;
 
                         Console.WriteLine($"the mean is : sum {sum} / list memberCount {list.Count} = {((decimal)sum / (decimal)list.Count):##,#0.00}");
-                        break;
+                        break; // mean
 
-                    case "s":
+                    case "sm":
                         int smallest = 0;
                         for (int i = 0; i < list.Count; i++)
                         {
@@ -86,8 +200,7 @@
                         Console.WriteLine(smallest);
 
 
-                        break;
-
+                        break; // smalest value
 
                     case "l":
                         int Largest = 0;
@@ -107,7 +220,7 @@
                         Console.WriteLine(Largest);
 
 
-                        break;
+                        break; // largest value
 
                     case "q":
                         Console.WriteLine("would you like to continue : (y/n)");
@@ -117,8 +230,32 @@
                         if (UsrInput.ToLower() == "y")
                         {
                             UserWillToContinue = false;
+                            Console.WriteLine("Goodbye");
                         }
-                        break;
+                        break; // quit
+
+                    case "s": // search
+                        Console.WriteLine("please Enter the List Member value that you want to search : ");
+                        int usrInputValue = Convert.ToInt32(Console.ReadLine());
+
+                        string MatchingStringIndex = "";
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            if (list[i] == usrInputValue)
+                            {
+                                MatchingStringIndex += $" {i}";
+                            }
+                            //else Console.WriteLine("Not found !");
+
+                        }
+
+
+
+
+                        Console.Write($"value found at index of  :{MatchingStringIndex}");
+
+
+                        break; // search
 
                     case "sa":
                         for (int i = 0; i < list.Count - 1; i++)
@@ -145,7 +282,7 @@
 
 
 
-        
+
 
 
                             }
@@ -160,7 +297,8 @@
                             Console.Write($"{item} ");
                         }
 
-                        break;
+                        break; // sort ascendent
+                   
                     case "sd":
                         for (int i = 0; i < list.Count - 1; i++)
                         {
@@ -204,21 +342,45 @@
                         }
 
 
-                        break;
+                        break; // sort descendent
 
+                    case "c":
+                        Console.WriteLine("Are You Sure , you Want to clear the List ? (Y _ N)");
+                        string usrinput = Console.ReadLine();
+
+                        
+
+                        if (usrinput.ToLower() == "y")
+                        {
+                            if (list.Count == 0) Console.WriteLine(" The List Is Empty" );
+                            else
+                            {
+                                list.Clear();
+                                //for (int i = 0; i < list.Count-1; i++)
+                                //{
+                                //    Console.WriteLine($"i{i} _ list[i]:{list[i]}" );
+                                //    Console.ForegroundColor = ConsoleColor.Red;
+                                //    list.Remove(list[i]);
+                                //    Console.WriteLine($"{list[i] } _ removed");
+                                //    Console.ForegroundColor = ConsoleColor.White; 
+                                //}
+
+                                for (int i = 0; i < list.Count; i++)
+                                {
+                                    Console.WriteLine(list[i]);
+                                }
+
+                                Console.WriteLine("list cleared ");
+                            }
+     
+                        }
+                        else Console.WriteLine("action ignored ");
+                        break; // clear
                     default:
-                        Console.WriteLine("Invalid Input ");
-
+                        Console.WriteLine("Unknown selection, please try again");
                         break;
-
-
-
                 }
             }
-
-
-
-
         }
     }
 }
